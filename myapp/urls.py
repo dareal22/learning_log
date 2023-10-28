bfafafefafd5943b75d37cbc3416318e6529cd40
dareal22 <darryl.chiu01@gmail.com>
2023-10-28 18:21:48 +0800

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for learning_log project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "myapp"
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+
+]
