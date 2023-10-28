91f130b46ef381b872e59b9a3d485b884a79403f
dareal22 <darryl.chiu01@gmail.com>
2023-10-28 18:46:08 +0800

    first commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-px^)6dt)kihmskt50341b&dt*#b#bsx5_6#!0v3wrn-a79-n)o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
